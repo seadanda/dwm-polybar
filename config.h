922068d895c95014b4f69ad438203071def574d9
Dónal Murray <donal.murray@cern.ch>
2020-09-21 13:00:53 +0200

    Add new polybar script

--- config.h
+++ config.h
@@ -3,16 +3,14 @@
 #include <X11/XF86keysym.h> // media keys
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
 static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
 static const char *alttrayname      = "tray";    /* Polybar tray instance name */
-static const char *altbarcmd        = "$HOME/bar.sh"; /* Alternate bar launch command */
-
-
+static const char *altbarcmd        = "$HOME/.config/polybar/launch.sh"; /* Alternate bar launch command */
 static const char *fonts[]          = {
     "noto:size=14",
 	"Font Awesome 5 Free Regular-12",
