3f80dd8b886a8ff8e09e61458933b84d6485b1c3
Dónal Murray <donal.murray@cern.ch>
2020-12-16 11:00:59 +0000

    Change tag labels to all icons

--- config.h
+++ config.h
@@ -30,7 +30,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "", "", ""};
+static const char *tags[] = { "", "", "", "", "", "", "", "", ""};
 
 static const Rule rules[] = {
     /* xprop(1):
